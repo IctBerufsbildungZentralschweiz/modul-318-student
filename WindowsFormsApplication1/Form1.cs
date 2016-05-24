1051ecb93831a933b9c8fd2710686928be40761a
dok406 <dok406@vfi.local>
2016-05-24 14:47:21 +0200

    project added

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,23 @@
+﻿using SwissTransport;
+using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("asdasdf");
+
+            Transport transport = new Transport();
+
+            MessageBox.Show(transport.ToString());
+        }
+    }
+}

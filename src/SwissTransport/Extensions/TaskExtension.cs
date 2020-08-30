590c34eb381f8023ae3cb30ea31708df5f62a33e
sirh3e <marvin.henk.pro@gmail.com>
2020-08-30 14:13:16 +0200

    Added TaskExtension.cs for getting the result from tasks

---
+++ src/SwissTransport/Extensions/TaskExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SwissTransport.Extensions
+{
+    public static class TaskExtension
+    {
+        public static T GetResult<T>(this Task<T> task)
+        {
+            if (task is null)
+                throw new ArgumentNullException(nameof(task));
+
+            task.Wait();
+            return task.Result;
+        }
+    }
+}

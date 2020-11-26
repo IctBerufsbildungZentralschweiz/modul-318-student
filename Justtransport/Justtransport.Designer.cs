133ea27a9a0583d7dfcf3f68ad5d89b8931ae2f5
Justine Kaeslin <jkaeslin@edp.ch>
2020-11-26 13:07:05 +0100

    UI erstellt

--- Justtransport/Justtransport.Designer.cs
+++ Justtransport/Justtransport.Designer.cs
@@ -28,19 +28,361 @@
     /// </summary>
     private void InitializeComponent()
     {
+      this.tabControl1 = new System.Windows.Forms.TabControl();
+      this.tabPage1 = new System.Windows.Forms.TabPage();
+      this.tabPage2 = new System.Windows.Forms.TabPage();
+      this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+      this.radioButton3 = new System.Windows.Forms.RadioButton();
+      this.radioButton4 = new System.Windows.Forms.RadioButton();
+      this.tableLayoutPanel5 = new System.Windows.Forms.TableLayoutPanel();
+      this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+      this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+      this.tableLayoutPanel6 = new System.Windows.Forms.TableLayoutPanel();
+      this.textBox3 = new System.Windows.Forms.TextBox();
+      this.button3 = new System.Windows.Forms.Button();
+      this.button4 = new System.Windows.Forms.Button();
+      this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+      this.radioButton2 = new System.Windows.Forms.RadioButton();
+      this.radioButton1 = new System.Windows.Forms.RadioButton();
+      this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+      this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+      this.datepicker = new System.Windows.Forms.DateTimePicker();
+      this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+      this.textBox2 = new System.Windows.Forms.TextBox();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.button1 = new System.Windows.Forms.Button();
+      this.button2 = new System.Windows.Forms.Button();
+      this.tabControl1.SuspendLayout();
+      this.tabPage1.SuspendLayout();
+      this.tabPage2.SuspendLayout();
+      this.tableLayoutPanel4.SuspendLayout();
+      this.tableLayoutPanel5.SuspendLayout();
+      this.tableLayoutPanel6.SuspendLayout();
+      this.tableLayoutPanel3.SuspendLayout();
+      this.tableLayoutPanel2.SuspendLayout();
+      this.tableLayoutPanel1.SuspendLayout();
       this.SuspendLayout();
+      // 
+      // tabControl1
+      // 
+      this.tabControl1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+      this.tabControl1.Controls.Add(this.tabPage1);
+      this.tabControl1.Controls.Add(this.tabPage2);
+      this.tabControl1.Location = new System.Drawing.Point(5, 4);
+      this.tabControl1.Multiline = true;
+      this.tabControl1.Name = "tabControl1";
+      this.tabControl1.SelectedIndex = 0;
+      this.tabControl1.Size = new System.Drawing.Size(789, 347);
+      this.tabControl1.TabIndex = 0;
+      // 
+      // tabPage1
+      // 
+      this.tabPage1.BackColor = System.Drawing.Color.Gainsboro;
+      this.tabPage1.Controls.Add(this.tableLayoutPanel3);
+      this.tabPage1.Controls.Add(this.tableLayoutPanel2);
+      this.tabPage1.Controls.Add(this.tableLayoutPanel1);
+      this.tabPage1.Controls.Add(this.button2);
+      this.tabPage1.Location = new System.Drawing.Point(4, 29);
+      this.tabPage1.Name = "tabPage1";
+      this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+      this.tabPage1.Size = new System.Drawing.Size(781, 314);
+      this.tabPage1.TabIndex = 0;
+      this.tabPage1.Text = "Start und Ende";
+      // 
+      // tabPage2
+      // 
+      this.tabPage2.BackColor = System.Drawing.Color.Gainsboro;
+      this.tabPage2.Controls.Add(this.tableLayoutPanel4);
+      this.tabPage2.Controls.Add(this.tableLayoutPanel5);
+      this.tabPage2.Controls.Add(this.tableLayoutPanel6);
+      this.tabPage2.Controls.Add(this.button4);
+      this.tabPage2.Location = new System.Drawing.Point(4, 29);
+      this.tabPage2.Name = "tabPage2";
+      this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+      this.tabPage2.Size = new System.Drawing.Size(781, 314);
+      this.tabPage2.TabIndex = 1;
+      this.tabPage2.Text = "Abfahrt von";
+      // 
+      // tableLayoutPanel4
+      // 
+      this.tableLayoutPanel4.ColumnCount = 2;
+      this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.14837F));
+      this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 49.85163F));
+      this.tableLayoutPanel4.Controls.Add(this.radioButton3, 0, 0);
+      this.tableLayoutPanel4.Controls.Add(this.radioButton4, 1, 0);
+      this.tableLayoutPanel4.Location = new System.Drawing.Point(411, 122);
+      this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+      this.tableLayoutPanel4.RowCount = 1;
+      this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel4.Size = new System.Drawing.Size(339, 38);
+      this.tableLayoutPanel4.TabIndex = 15;
+      // 
+      // radioButton3
+      // 
+      this.radioButton3.AutoSize = true;
+      this.radioButton3.Location = new System.Drawing.Point(3, 3);
+      this.radioButton3.Name = "radioButton3";
+      this.radioButton3.Size = new System.Drawing.Size(87, 24);
+      this.radioButton3.TabIndex = 4;
+      this.radioButton3.TabStop = true;
+      this.radioButton3.Text = "Abfahrt";
+      this.radioButton3.UseVisualStyleBackColor = true;
+      // 
+      // radioButton4
+      // 
+      this.radioButton4.AutoSize = true;
+      this.radioButton4.Location = new System.Drawing.Point(173, 3);
+      this.radioButton4.Name = "radioButton4";
+      this.radioButton4.Size = new System.Drawing.Size(90, 24);
+      this.radioButton4.TabIndex = 4;
+      this.radioButton4.TabStop = true;
+      this.radioButton4.Text = "Ankunft";
+      this.radioButton4.UseVisualStyleBackColor = true;
+      // 
+      // tableLayoutPanel5
+      // 
+      this.tableLayoutPanel5.ColumnCount = 1;
+      this.tableLayoutPanel5.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel5.Controls.Add(this.dateTimePicker2, 0, 1);
+      this.tableLayoutPanel5.Controls.Add(this.dateTimePicker3, 0, 0);
+      this.tableLayoutPanel5.Location = new System.Drawing.Point(411, 13);
+      this.tableLayoutPanel5.Name = "tableLayoutPanel5";
+      this.tableLayoutPanel5.RowCount = 2;
+      this.tableLayoutPanel5.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel5.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel5.Size = new System.Drawing.Size(339, 103);
+      this.tableLayoutPanel5.TabIndex = 14;
+      // 
+      // dateTimePicker2
+      // 
+      this.dateTimePicker2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+      this.dateTimePicker2.Location = new System.Drawing.Point(174, 54);
+      this.dateTimePicker2.Name = "dateTimePicker2";
+      this.dateTimePicker2.Size = new System.Drawing.Size(162, 26);
+      this.dateTimePicker2.TabIndex = 3;
+      // 
+      // dateTimePicker3
+      // 
+      this.dateTimePicker3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.dateTimePicker3.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+      this.dateTimePicker3.Location = new System.Drawing.Point(174, 3);
+      this.dateTimePicker3.Name = "dateTimePicker3";
+      this.dateTimePicker3.Size = new System.Drawing.Size(162, 26);
+      this.dateTimePicker3.TabIndex = 2;
+      // 
+      // tableLayoutPanel6
+      // 
+      this.tableLayoutPanel6.ColumnCount = 1;
+      this.tableLayoutPanel6.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel6.Controls.Add(this.textBox3, 0, 0);
+      this.tableLayoutPanel6.Controls.Add(this.button3, 0, 2);
+      this.tableLayoutPanel6.Location = new System.Drawing.Point(30, 13);
+      this.tableLayoutPanel6.Name = "tableLayoutPanel6";
+      this.tableLayoutPanel6.RowCount = 3;
+      this.tableLayoutPanel6.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 17.67241F));
+      this.tableLayoutPanel6.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 82.32758F));
+      this.tableLayoutPanel6.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 55F));
+      this.tableLayoutPanel6.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableLayoutPanel6.Size = new System.Drawing.Size(343, 288);
+      this.tableLayoutPanel6.TabIndex = 13;
+      // 
+      // textBox3
+      // 
+      this.textBox3.Location = new System.Drawing.Point(3, 3);
+      this.textBox3.Name = "textBox3";
+      this.textBox3.Size = new System.Drawing.Size(279, 26);
+      this.textBox3.TabIndex = 1;
+      // 
+      // button3
+      // 
+      this.button3.BackColor = System.Drawing.Color.Silver;
+      this.button3.Location = new System.Drawing.Point(3, 235);
+      this.button3.Name = "button3";
+      this.button3.Size = new System.Drawing.Size(279, 36);
+      this.button3.TabIndex = 0;
+      this.button3.Text = "Verbindungen anzeigen";
+      this.button3.UseVisualStyleBackColor = false;
+      // 
+      // button4
+      // 
+      this.button4.BackColor = System.Drawing.Color.Silver;
+      this.button4.Location = new System.Drawing.Point(411, 248);
+      this.button4.Name = "button4";
+      this.button4.Size = new System.Drawing.Size(336, 36);
+      this.button4.TabIndex = 12;
+      this.button4.Text = "Karte anzeigen";
+      this.button4.UseVisualStyleBackColor = false;
+      // 
+      // tableLayoutPanel3
+      // 
+      this.tableLayoutPanel3.ColumnCount = 2;
+      this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.14837F));
+      this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 49.85163F));
+      this.tableLayoutPanel3.Controls.Add(this.radioButton2, 0, 0);
+      this.tableLayoutPanel3.Controls.Add(this.radioButton1, 1, 0);
+      this.tableLayoutPanel3.Location = new System.Drawing.Point(411, 122);
+      this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+      this.tableLayoutPanel3.RowCount = 1;
+      this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel3.Size = new System.Drawing.Size(339, 38);
+      this.tableLayoutPanel3.TabIndex = 15;
+      // 
+      // radioButton2
+      // 
+      this.radioButton2.AutoSize = true;
+      this.radioButton2.Location = new System.Drawing.Point(3, 3);
+      this.radioButton2.Name = "radioButton2";
+      this.radioButton2.Size = new System.Drawing.Size(87, 24);
+      this.radioButton2.TabIndex = 4;
+      this.radioButton2.TabStop = true;
+      this.radioButton2.Text = "Abfahrt";
+      this.radioButton2.UseVisualStyleBackColor = true;
+      // 
+      // radioButton1
+      // 
+      this.radioButton1.AutoSize = true;
+      this.radioButton1.Location = new System.Drawing.Point(173, 3);
+      this.radioButton1.Name = "radioButton1";
+      this.radioButton1.Size = new System.Drawing.Size(90, 24);
+      this.radioButton1.TabIndex = 4;
+      this.radioButton1.TabStop = true;
+      this.radioButton1.Text = "Ankunft";
+      this.radioButton1.UseVisualStyleBackColor = true;
+      // 
+      // tableLayoutPanel2
+      // 
+      this.tableLayoutPanel2.ColumnCount = 1;
+      this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel2.Controls.Add(this.dateTimePicker1, 0, 1);
+      this.tableLayoutPanel2.Controls.Add(this.datepicker, 0, 0);
+      this.tableLayoutPanel2.Location = new System.Drawing.Point(411, 13);
+      this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+      this.tableLayoutPanel2.RowCount = 2;
+      this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel2.Size = new System.Drawing.Size(339, 103);
+      this.tableLayoutPanel2.TabIndex = 14;
+      // 
+      // dateTimePicker1
+      // 
+      this.dateTimePicker1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+      this.dateTimePicker1.Location = new System.Drawing.Point(174, 54);
+      this.dateTimePicker1.Name = "dateTimePicker1";
+      this.dateTimePicker1.Size = new System.Drawing.Size(162, 26);
+      this.dateTimePicker1.TabIndex = 3;
+      // 
+      // datepicker
+      // 
+      this.datepicker.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.datepicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+      this.datepicker.Location = new System.Drawing.Point(174, 3);
+      this.datepicker.Name = "datepicker";
+      this.datepicker.Size = new System.Drawing.Size(162, 26);
+      this.datepicker.TabIndex = 2;
+      // 
+      // tableLayoutPanel1
+      // 
+      this.tableLayoutPanel1.ColumnCount = 1;
+      this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+      this.tableLayoutPanel1.Controls.Add(this.textBox2, 0, 0);
+      this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 1);
+      this.tableLayoutPanel1.Controls.Add(this.button1, 0, 2);
+      this.tableLayoutPanel1.Location = new System.Drawing.Point(30, 13);
+      this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+      this.tableLayoutPanel1.RowCount = 3;
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 17.67241F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 82.32758F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 55F));
+      this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableLayoutPanel1.Size = new System.Drawing.Size(343, 288);
+      this.tableLayoutPanel1.TabIndex = 13;
+      // 
+      // textBox2
+      // 
+      this.textBox2.Location = new System.Drawing.Point(3, 3);
+      this.textBox2.Name = "textBox2";
+      this.textBox2.Size = new System.Drawing.Size(279, 26);
+      this.textBox2.TabIndex = 1;
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(3, 44);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(279, 26);
+      this.textBox1.TabIndex = 1;
+      // 
+      // button1
+      // 
+      this.button1.BackColor = System.Drawing.Color.Silver;
+      this.button1.Location = new System.Drawing.Point(3, 235);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(279, 36);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "Verbindungen anzeigen";
+      this.button1.UseVisualStyleBackColor = false;
+      // 
+      // button2
+      // 
+      this.button2.BackColor = System.Drawing.Color.Silver;
+      this.button2.Location = new System.Drawing.Point(411, 248);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(336, 36);
+      this.button2.TabIndex = 12;
+      this.button2.Text = "Karte anzeigen";
+      this.button2.UseVisualStyleBackColor = false;
       // 
       // Justtransport
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(800, 359);
+      this.Controls.Add(this.tabControl1);
       this.Name = "Justtransport";
       this.Text = "Justtransport";
+      this.tabControl1.ResumeLayout(false);
+      this.tabPage1.ResumeLayout(false);
+      this.tabPage2.ResumeLayout(false);
+      this.tableLayoutPanel4.ResumeLayout(false);
+      this.tableLayoutPanel4.PerformLayout();
+      this.tableLayoutPanel5.ResumeLayout(false);
+      this.tableLayoutPanel6.ResumeLayout(false);
+      this.tableLayoutPanel6.PerformLayout();
+      this.tableLayoutPanel3.ResumeLayout(false);
+      this.tableLayoutPanel3.PerformLayout();
+      this.tableLayoutPanel2.ResumeLayout(false);
+      this.tableLayoutPanel1.ResumeLayout(false);
+      this.tableLayoutPanel1.PerformLayout();
       this.ResumeLayout(false);
 
     }
 
     #endregion
+
+    private System.Windows.Forms.TabControl tabControl1;
+    private System.Windows.Forms.TabPage tabPage1;
+    private System.Windows.Forms.TabPage tabPage2;
+    private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+    private System.Windows.Forms.RadioButton radioButton3;
+    private System.Windows.Forms.RadioButton radioButton4;
+    private System.Windows.Forms.TableLayoutPanel tableLayoutPanel5;
+    private System.Windows.Forms.DateTimePicker dateTimePicker2;
+    private System.Windows.Forms.DateTimePicker dateTimePicker3;
+    private System.Windows.Forms.TableLayoutPanel tableLayoutPanel6;
+    private System.Windows.Forms.TextBox textBox3;
+    private System.Windows.Forms.Button button3;
+    private System.Windows.Forms.Button button4;
+    private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+    private System.Windows.Forms.RadioButton radioButton2;
+    private System.Windows.Forms.RadioButton radioButton1;
+    private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+    private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    private System.Windows.Forms.DateTimePicker datepicker;
+    private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    private System.Windows.Forms.TextBox textBox2;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Button button2;
   }
 }
